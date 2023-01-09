c210e1bf2b724f35ef08f40ddee3f8395f69d7bf
kureung <kureung@hanpass.com>
2023-01-09 15:29:58 +0900

    refactor(DefaultLoggingStrategy): 테스트 수정

--- src/test/java/io/github/kureung/logging/log/interceptor/strategy/DefaultLoggingStrategyTest.java
+++ src/test/java/io/github/kureung/logging/log/interceptor/strategy/DefaultLoggingStrategyTest.java
@@ -5,7 +5,6 @@ import static org.assertj.core.api.Assertions.assertThat;
 import ch.qos.logback.classic.Logger;
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import ch.qos.logback.core.read.ListAppender;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import io.github.kureung.logging.log.filter.ContentCachingFilter;
 import java.io.IOException;
 import java.util.List;
@@ -22,7 +21,6 @@ import org.springframework.mock.web.MockHttpServletResponse;
 class DefaultLoggingStrategyTest {
     private Logger logger;
     private MockFilterChain filterChain;
-    private final ObjectMapper objectMapper = new ObjectMapper();
 
     @BeforeEach
     void setUp() throws ServletException, IOException {
@@ -40,7 +38,7 @@ class DefaultLoggingStrategyTest {
         listAppender.start();
         logger.addAppender(listAppender);
 
-        final DefaultLoggingStrategy defaultLoggingStrategy = new DefaultLoggingStrategy(objectMapper);
+        final DefaultLoggingStrategy defaultLoggingStrategy = new DefaultLoggingStrategy();
         defaultLoggingStrategy.execute((HttpServletRequest) filterChain.getRequest(), (HttpServletResponse) filterChain.getResponse());
 
         List<ILoggingEvent> logs = listAppender.list;
